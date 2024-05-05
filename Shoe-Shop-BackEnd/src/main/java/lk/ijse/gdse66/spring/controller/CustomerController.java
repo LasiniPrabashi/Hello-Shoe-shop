38dcca9fdf5e2dc06f4a7dae2f2df0ad4483ed87
LasiniPrabashi <lasiniprabashi0327@gmail.com>
2024-05-05 12:37:45 +0530

    item entity update

--- Shoe-Shop-BackEnd/src/main/java/lk/ijse/gdse66/spring/controller/CustomerController.java
+++ Shoe-Shop-BackEnd/src/main/java/lk/ijse/gdse66/spring/controller/CustomerController.java
@@ -1,2 +1,56 @@
-package lk.ijse.gdse66.spring.controller;public class CustomerController {
+package lk.ijse.gdse66.spring.controller;
+
+import lk.ijse.gdse66.spring.dto.CustomDTO;
+import lk.ijse.gdse66.spring.dto.CustomerDTO;
+import lk.ijse.gdse66.spring.embeded.Address;
+import lk.ijse.gdse66.spring.service.CustomerService;
+import lk.ijse.gdse66.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService service;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public ResponseUtil saveCustomer(@ModelAttribute CustomerDTO dto, Address address){
+        System.out.println(dto.toString());
+        dto.setAddress(address);
+        service.saveCustomer(dto);
+        return new ResponseUtil("200", "Successfully Registered.!", null);
+    }
+
+    @GetMapping
+    public ResponseUtil getAllCustomer(){
+        return new ResponseUtil("200", "Successfully Loaded. :", service.loadAllCustomer());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/cusIdGenerate")
+    public @ResponseBody
+    CustomDTO customerIdGenerate() {
+        return service.customerIdGenerate();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PutMapping
+    public ResponseUtil updateCustomer(@ModelAttribute CustomerDTO customerDTO, Address address){
+        customerDTO.setAddress(address);
+        service.updateCustomer(customerDTO);
+        return new ResponseUtil("200", "Successfully Updated. :"+ customerDTO.getCode(),null);
+
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @DeleteMapping
+    public ResponseUtil deleteCustomer(@RequestParam String code){
+        service.deleteCustomer(code);
+        return new ResponseUtil("200", "Successfully Deleted. :"+ code,null);
+         }
 }
--- Shoe-Shop-BackEnd/src/main/java/lk/ijse/gdse66/spring/entity/Item.java
+++ Shoe-Shop-BackEnd/src/main/java/lk/ijse/gdse66/spring/entity/Item.java
@@ -1,7 +1,6 @@
 package lk.ijse.gdse66.spring.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,4 +12,18 @@ import lombok.NoArgsConstructor;
 public class Item {
     @Id
     private String code;
+    private String Name;
+    private Integer qty;
+    @Column(columnDefinition = "LONGTEXT")
+    private String itemPicture;
+    private String category;
+    private Integer size;
+    @ManyToOne(cascade = {CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "supplier_id", referencedColumnName = "code", nullable = false)
+    private Supplier supplier;
+    private Double salePrice;
+    private Double buyPrice;
+    private Double expectedProfit;
+    private Double profitMargin;
+    private String status;
 }
